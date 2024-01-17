5e890589ff7365c8ad2866991f449e0e841d12cb
Ducky <bobrov.utka2004@yandex.ru>
2024-01-18 00:19:41 +0300

    Minor updates
    - Add version display while playing
    - Delete .net project files

---
+++ scripts/Version display.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var contained_text : String = "TEST"
+	
+	contained_text = "game version: " + ProjectSettings.get_setting("application/config/version")
+	
+	$MarginContainer/Label.text = contained_text
